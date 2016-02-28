ae851992a6cf693439bcd5e545b3c7eef5ee36c2
ODROID <roman-dvorak@email.cz>
2016-02-28 20:29:28 +0100

    pymlab integration

--- src/SensorServer/scripts/client.py
+++ src/SensorServer/scripts/client.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
 import sys
+import pymlab
 import rospy
+import time
+from pymlab import config
 import sensor_server
+from std_msgs.msg import String
 from sensor_server.srv import *
 
-def add_two_ints_client(x, y):
-    rospy.wait_for_service('pymlab')
-    print "aaa"
-    try:
-        pymlab = rospy.ServiceProxy('pymlab', GetSensVal)
-        odpoved = pymlab(x, y)
-        return odpoved.val
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-
-def usage():
-    return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        x = str(sys.argv[1])
-        y = str(sys.argv[2])
-    else:
-        print usage()
-        sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
-    print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
+    i2c = '''{
+            "port": 1,
+        }'''
+    bus = '''[
+                {
+                    "name":           "lts01",
+                    "type":           "lts01",
+                },
+            ]'''
+    pymlab_init = rospy.ServiceProxy('pymlab_init', PymlabInit, headers= {'i2c':i2c, 'bus':bus} )
+    pymlab = rospy.ServiceProxy('pymlab', GetSensVal)
+    rospy.init_node('pymlab_client', anonymous=True)
+
+    rospy.loginfo("init")
+    time.sleep(1)
+    odpoved = pymlab_init(i2c=i2c, bus=bus)
+    print odpoved
+    odpoved = pymlab("ahoj", "maje")
+
+    print "%s + %s = %s"%(2, 1, odpoved)
--- src/SensorServer/scripts/sensorserver_example.py
+++ src/SensorServer/scripts/sensorserver_example.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import rospy
+import pymlab
+from pymlab import config
+import sys
 import sensor_server
+#from std_msgs.msg import String
+import std_msgs
 from sensor_server.srv import *
 
 def server(req):
@@ -9,9 +14,38 @@ def server(req):
     print "Returning [%s + %s]"%(req.name, req.data)
     return GetSensValResponse( 10 )
 
+class pymlab_server():
+    def __init__(self):
+        pass
+
+    def init(self, cfg=None):
+        print cfg
+        i2c = {
+                "port": 1,
+            }
+        bus = [
+                    {
+                        "name":           "lts01",
+                        "type":           "lts01",
+                    },
+                ]
+        self.pymlab_config = config.Config(i2c = eval(cfg.i2c), bus = eval(cfg.bus))
+        self.pymlab_config.initialize()
+        self.lts_sen = self.pymlab_config.get_device("lts01")
+        print "done"
+        return True
+
+    def getvalue(self, name=None, type=None, value=None):
+        print name, type, value
+        val = int(float(self.lts_sen.get_temp()))
+        print "value je tohle:", val
+        return GetSensValResponse(val)
+
 def add_two_ints_server():
+    ps = pymlab_server()
     rospy.init_node('pymlab_node')
-    s = rospy.Service('pymlab', GetSensVal, server)
+    s1 = rospy.Service('pymlab_init', PymlabInit, ps.init)
+    s2 = rospy.Service('pymlab', GetSensVal, ps.getvalue)
     print "Ready to add two ints."
     rospy.spin()
 
