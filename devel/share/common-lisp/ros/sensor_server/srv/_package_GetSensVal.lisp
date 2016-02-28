c84dd24603fa64bfcfd5d7622c9abb41e81bdb69
ODROID <roman-dvorak@email.cz>
2016-02-28 14:11:44 +0100

    clean

--- devel/share/common-lisp/ros/sensor_server/srv/_package_GetSensVal.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package sensor_server-srv)
-(cl:export '(NAME-VAL
-          NAME
-          DATA-VAL
-          DATA
-          VAL-VAL
-          VAL
-))
--- devel/share/sensor_server/cmake/sensor_server-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(sensor_server_MESSAGE_FILES "")
-set(sensor_server_SERVICE_FILES "/home/roman/roman-notebook/repos/ROS_sensor-ui/src/SensorServer/srv/GetSensVal.srv")
